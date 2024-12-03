724a5e3a70c9e57206d70c3628d81c5ef09c23da
Cesar Santos <csar.santos18@gmail.com>
2024-12-03 11:57:12 -0500

    Second  commit

--- src/arrays.js
+++ src/arrays.js
@@ -3,3 +3,19 @@ const redesSociais= ["Facebook", "Instagram", "X"];
 for (let i = 0; i < redesSociais.length; i++){
     console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`)
 }
+
+redesSociais.forEach(function(nomeDaRedeSocial, indice) {
+    console.log(` #${indice} Eu tenho perfil na rede social: ${nomeDaRedeSocial}`)
+});
+
+const alunos = ['Gustavo', 'Julia', 'Paula', 'Wagner'];
+
+
+
+const alunos2 = alunos.map(function(itemAtual){
+    itemAtual = {
+        nome: itemAtual,
+        curso: 'Front-End'
+    }
+    return itemAtual;
+})
